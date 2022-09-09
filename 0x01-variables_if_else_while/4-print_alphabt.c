662fa21b3227c5884c5f26fbe633c47913f7a102
TseyiMayuku <mayukutseyi@gmail.com>
2022-09-09 10:42:38 -0700

    When I was having that alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	
+	{
+		e = 'e'; 
+		q = 'q';
+
+		if (low != e; && low != q)
+		putchar(low);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
