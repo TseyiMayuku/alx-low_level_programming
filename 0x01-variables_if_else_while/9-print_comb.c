7459f02035c8c0383dea0580e1e18b5523c734ea
TseyiMayuku <mayukutseyi@gmail.com>
2022-09-09 12:38:00 -0700

    unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,13 +14,13 @@ int main(void)
 	int d;
 
 	for (d = '0'; d <= '9'; d++)
-	putchar(d);
 
 		{
+			putchar(d);
 			if (d != '9')
 			{
-				putchar(',');
 				putchar(' ');
+				putchar(',');
 			}
 
 		}
