c977a9c830d8f337552bea74e132c8d8448f8874
TseyiMayuku <mayukutseyi@gmail.com>
2022-09-15 17:39:27 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,28 +15,27 @@ int main(void)
 
 	{
 		if (i % 3 == 0 && (i % 5 == 0)
-		{
+
 			printf("%s", "FizzBuzz");
-		}
+
 
 		else if (i % 3 == 0)
-		{
+
 			printf("%s", "Fizz");
-		}
+
 
 		else if (i % 5 == 0)
-		{
+
 			printf("%s", "Buzz");
-		}
 
 		else
-		{
+
 			printf("%d", i);
-		}
+
 		if (i != 100)
-		{
+
 			printf(" ");
-		}
+
 	}
 	printf("\n");
 
