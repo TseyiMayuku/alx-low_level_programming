abdef9b932b4ad7cf5e1cf196b41537de2213446
TseyiMayuku <mayukutseyi@gmail.com>
2022-12-12 17:36:13 -0800

    main header file

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,11 @@
 #ifndef _MAIN_
 #define _MAIN_
 
+int add(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
